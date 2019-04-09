17b1175d5e6eba348f9ea934a2451c1609b625f1
seven0717 <seven_s@aliyun.com>
2019-04-10 02:27:01 +0800

    post

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,10 +1,10 @@
-import React,{ Component } from 'react';
+import React, {Component} from 'react';
 
 export default class App extends Component {
-   render(){
+   render() {
       return (
          <div>
-            { this.props.children }
+            {this.props.children}
          </div>
       )
    }
